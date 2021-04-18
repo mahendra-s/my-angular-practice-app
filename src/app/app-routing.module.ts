23408ba946cd5339a93b080b224ff5143ea9b8a1
Mahendra Shinde <rahul.mahendra2009@gmail.com>
2021-04-18 12:24:16 +0530

    template example added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { EventBindingExampleComponent } from './event-binding-example/event-binding-example.component';
+import { TemplateExampleComponent } from './template-example/template-example.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutMeComponent},
-  {path: 'event-binding-example', component: EventBindingExampleComponent}
+  {path: 'event-binding-example', component: EventBindingExampleComponent},
+  {path: 'template-example', component: TemplateExampleComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { EventBindingExampleComponent } from './event-binding-example/event-binding-example.component';
+import { TemplateExampleComponent } from './template-example/template-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutMeComponent,
-    EventBindingExampleComponent
+    EventBindingExampleComponent,
+    TemplateExampleComponent
   ],
   imports: [
     BrowserModule,
